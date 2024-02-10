03e269bed391e2dc1ad940d7ab769e70cb386127
piyal-nagda <136745417+piyal-nagda@users.noreply.github.com>
2024-02-10 11:06:11 +0530

    made modal usable

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -26,7 +26,7 @@ const Modal = ({ setModalOpen, contract }) => {
     <>
       <div className="modalBackground">
         <div className="modalContainer">
-          <div className="title">Share with</div>
+          <div className="title-share">Share with</div>
           <div className="body">
             <input
               type="text"
